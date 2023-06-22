97bca32d4960ce12922a39cf9e451c7b617784c3
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2023-06-21 23:16:14 -0300

    Add first content

---
+++ src/content/hero.content.js
@@ -0,0 +1,10 @@
+export const HERO_MESSAGE = {
+  title: 'Have an Epic Journey!',
+  messages: {
+    first: 'I just wanted to say how much I\'m gonna miss you. But don\'t worry, distance ain\'t got nothing on us!',
+    second: 'You\'re gonna rock the Au Pair life, exploring new horizons, meeting awesome people, and creating memories that\'ll last a lifetime. You\'ve got this!',
+    third: 'Can\'t wait to hear about all your adventures and see those breathtaking photos. I\'ll be cheering for you every step of the way.',
+    fourth: 'Wishing you an incredible year filled with love, joy, and epic experiences.',
+    highlight: 'This is the way.'
+  }
+}
