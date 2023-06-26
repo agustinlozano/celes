45085fb6d74b2dbd05b0081dc6e042baf0c5e472
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2023-06-26 09:08:40 -0300

    add build

--- src/components/HighlightText.jsx
+++
@@ -1,5 +0,0 @@
-function HighlightText ({ children, customClass }) {
-  return <span className={`text-neutral-700 text-5xl font-cute font-bold ${customClass}`}>{children}</span>
-}
-
-export default HighlightText
