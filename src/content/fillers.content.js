ffc6b8025589c06631a11103a560df1cbc270251
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2023-06-26 09:04:46 -0300

    Update and add content

---
+++ src/content/fillers.content.js
@@ -0,0 +1,17 @@
+export const FIRST = {
+  title: 'It\'s time to hit the road and get your USA groove on!',
+  message: 'You\'re about to embark on a journey that will change your life forever. You\'ll meet new people, see new places, and experience new things. You\'ll learn a lot about yourself and the world around you. You\'ll grow as a person and become more independent. You\'ll have the time of your life!',
+  icon: {
+    src: '/src/assets/img/what-ever.png',
+    alt: 'Add alt text'
+  }
+}
+
+export const SECOND = {
+  title: 'We\'ve conquered the Seven Kingdoms and soared through the stars together.',
+  message: 'We\'ve been through a lot together, and I\'m so proud of you for making it this far. I know you\'re going to do great things in the future, and I can\'t wait to see what you accomplish next. You\'re going to be amazing!',
+  icon: {
+    src: '/src/assets/img/what-ever.png',
+    alt: 'Add alt text'
+  }
+}
