b413a4e56a8d986b9b252ebaf1cd703ae56e5f89
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2023-06-27 22:33:37 -0300

    Edit asset paths

--- src/content/travels.content.js
+++ src/content/travels.content.js
@@ -6,42 +6,42 @@ export const MAIN = {
 export const PLACES = [
   {
     id: 1,
-    src: '/src/assets/photos/1.jpg',
+    src: '/assets/photos/1.jpg',
     place: 'San Francisco.',
     description: 'Stylish people are talking in front of an art deco building',
     featuredIn: ['Street Photography']
   },
   {
     id: 2,
-    src: '/src/assets/photos/2.jpg',
+    src: '/assets/photos/2.jpg',
     place: 'Tribune Tower, Chicago.',
     description: 'The facade of the Tribune Tower reflects architectural diversity, as it incorporates fragments and elements from various famous buildings around the world.',
     featuredIn: ['Architecture', 'Culture']
   },
   {
     id: 3,
-    src: '/src/assets/photos/3.jpg',
+    src: '/assets/photos/3.jpg',
     place: 'Red Hot Chili Peppers concert, somewhere.',
     description: 'Have fun, enjoy the music, and don\'t forget to take a photo!',
     featuredIn: ['Music', 'Events']
   },
   {
     id: 4,
-    src: '/src/assets/photos/4.jpg',
+    src: '/assets/photos/4.jpg',
     place: 'State House Bell, Philadelphia.',
     description: 'It\'s an iconic symbol of independence and lberty in the United States. The Liberty Bell is a bronze bell that was originally cast in 1752 in London, England.',
     featuredIn: ['Cultural places']
   },
   {
     id: 5,
-    src: '/src/assets/photos/5.jpg',
+    src: '/assets/photos/5.jpg',
     place: 'Time Square, NYC.',
     description: 'A place when you can see how fast the world is moving',
     featuredIn: ['Cool photos']
   },
   {
     id: 6,
-    src: '/src/assets/photos/6.jpg',
+    src: '/assets/photos/6.jpg',
     place: 'Timberline Lodge, Oregon.',
     description: 'It\'s the building that was used for the exterior shots of the Overlook Hotel in Stanley Kubrick\'s 1980 film The Shining.',
     featuredIn: ['Movies', 'Iconic places']
