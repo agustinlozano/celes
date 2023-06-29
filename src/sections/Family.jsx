cbb22cea33bb6d22b51f70fe9e31530232c60da0
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2023-06-28 22:59:08 -0300

    Update sections

--- src/sections/Family.jsx
+++ src/sections/Family.jsx
@@ -4,7 +4,7 @@ import RegularText from '../components/RegularText'
 function Memeber ({ name, img, description }) {
   return (
     <div className='mx-6 shadow-sm'>
-      <div className='border-l-4 border-b-2 border-r border-l-gray-600 rounded-sm pb-1'>
+      <div className='border-l-4 border-b-2 border-r border-l-gray-600 rounded-tl-md pb-1'>
         <img src={img} alt={name} className='rounded-sm' />
         <h3 className='text-2xl font-lato-bold mt-1'>{name}</h3>
         <RegularText className='px-1'>{description}</RegularText>
@@ -17,7 +17,7 @@ function FamilySection () {
   return (
     <section className='min-h-full pb-36'>
       <HighlightText>Your Funky Little Family</HighlightText>
-      <RegularText>This section is dedicated to the family that is waiting 4 you. Come here when you miss us.</RegularText>
+      <RegularText className='px-6'>This section is dedicated to the family that is waiting 4 you. Come here when you miss us.</RegularText>
       <div id='family-container' className='mt-3 flex flex-col gap-7'>
         <Memeber
           name='Niñito'
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -9,11 +9,11 @@ function ThreeDots () {
       initial={{ rotate: 0 }}
       animate={{ rotate: 180 }}
       transition={{ duration: 1, delay: 0.5 }}
-      className='flex flex-row justify-center items-center'
+      className='flex flex-row justify-center items-center mt-4'
     >
-      <div className='w-2 h-2 bg-slate-800 rounded-full ' />
-      <div className='w-2 h-2 bg-slate-800 rounded-full ml-2' />
-      <div className='w-2 h-2 bg-slate-800 rounded-full ml-2' />
+      <div className='w-1 h-1 bg-slate-800 rounded-full ' />
+      <div className='w-1 h-1 bg-slate-800 rounded-full ml-2' />
+      <div className='w-1 h-1 bg-slate-800 rounded-full ml-2' />
     </motion.div>
   )
 }
@@ -21,25 +21,23 @@ function ThreeDots () {
 function HeroMessage () {
   return (
     <div
-      id='hero' className='flex flex-col justify-center min-h-screen py-2 mb-20'
+      id='hero' className='flex flex-col justify-center min-h-screen mb-20'
     >
-      <h1 className='text-8xl font-cute'>Cele's letter</h1>
+      <h1 className='text-7xl font-cute'>Cele's letter</h1>
       <header id='my-message-section' className='mx-auto px-1'>
-        <h2 className='text-5xl font-cute mb-2'>{HERO_MESSAGE.title}</h2>
+        <h2 className='text-4xl font-cute'>{HERO_MESSAGE.title}</h2>
         <motion.div
           initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1.5, delay: 0.5 }}
+          className='mx-6'
         >
-          <RegularText>{HERO_MESSAGE.messages.first}</RegularText>
-          <br />
-          <RegularText>{HERO_MESSAGE.messages.second}</RegularText>
-          <br />
-          <RegularText>{HERO_MESSAGE.messages.third}</RegularText>
-          <br />
+          <RegularText className='mb-2'>{HERO_MESSAGE.messages.first}</RegularText>
+          <RegularText className='mb-2'>{HERO_MESSAGE.messages.second}</RegularText>
+          <RegularText className='mb-2'>{HERO_MESSAGE.messages.third}</RegularText>
           <RegularText>{HERO_MESSAGE.messages.fourth}</RegularText>
           <div className='mt-5'>
-            <img src='/assets/img/flag.png' alt='United States flag' className='w-36 mx-auto' />
+            <img src='/assets/img/flag.png' alt='United States flag' className='w-28 mx-auto' />
             <HighlightText
               customClass='block mt-5'
             >
