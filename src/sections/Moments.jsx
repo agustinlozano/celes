19d8f7d30d64107fbf163b127aef9cdbb0796215
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2023-07-01 21:47:28 -0300

    Add a new section

---
+++ src/sections/Moments.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react'
+import HighlightText from '../components/HighlightText.jsx'
+import RegularText from '../components/RegularText.jsx'
+import Image from '../components/atoms/Image.jsx'
+import useSectionVisibility from '../hooks/useSectionVisibility.jsx'
+import Loading from '../components/atoms/spinner/Loading.jsx'
+
+import { MOMENTS, MAIN } from '../content/moments.content.js'
+
+function LoadMore ({ onClick, hide }) {
+  if (hide) {
+    return null
+  }
+
+  return (
+    <div className='flex flex-row justify-center items-center my-5 mx-auto'>
+      <button className='bg-blue-950 hover:bg-blue-900 text-stone-100 font-bold py-2 px-4 rounded' onClick={onClick}>
+        Load more
+      </button>
+    </div>
+  )
+}
+
+function Moments () {
+  const [photos, setPhotos] = useState([])
+  const isSectionVisible = useSectionVisibility('moments')
+
+  useEffect(() => {
+    // add three photos to the state
+    setPhotos(MOMENTS.slice(0, 3))
+  }, [])
+
+  function handleLoadMore () {
+    // add three more photos to the state
+    setPhotos(MOMENTS.slice(0, photos.length + 3))
+  }
+
+  return (
+    <div id='moments' className='flex flex-col justify-center min-h-screen mb-20 mx-6'>
+      <HighlightText>{MAIN.title}</HighlightText>
+      <RegularText>{MAIN.message}</RegularText>
+      <div id='my-moments-section' className='mx-auto px-1 my-5'>
+        {isSectionVisible
+          ? (
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+              {photos && photos.map((place) => {
+                return (
+                  <div key={place.id} className='flex flex-col justify-start items-center border border-l-4 border-b-2 border-r border-l-blue-950 rounded-tl-md'>
+                    <Image src={place.src} description={place.alt} />
+                    <div className='my-2'>
+                      <RegularText>{place.description}</RegularText>
+                    </div>
+                  </div>
+                )
+              })}
+              <LoadMore
+                onClick={handleLoadMore}
+                hide={photos.length === MOMENTS.length}
+              />
+            </div>)
+          : <Loading />}
+      </div>
+    </div>
+  )
+}
+
+export default Moments
